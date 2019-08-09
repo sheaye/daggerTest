e7d541ff7d730c30fc7ae11398da3ad8a24dad86
yexinyan <yexinyan@migu.cn>
2019-08-09 12:06:34 +0800

    测试工厂类和注入器

--- DaggerDemo3/app/src/main/java/com/sheaye/test/MainActivity.java
+++ DaggerDemo3/app/src/main/java/com/sheaye/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.sheaye.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import javax.inject.Inject;
 
@@ -17,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final String tag = getClass().getName();
+//        final DripCoffeeModule module = new DripCoffeeModule();
+//        MainActivity_MembersInjector.injectMHeater(this,
+//                DripCoffeeModule_ProvideHeaterFactory.provideHeater(module));
+//        MainActivity_MembersInjector.injectMPump(this,
+//                DripCoffeeModule_ProvidePumpFactory.providePump(module,
+//                        module.providePump(
+//                                new Thermosiphon(DripCoffeeModule_ProvideHeaterFactory.provideHeater(module)))));
+//        Log.e(tag, mHeater.toString());
+//        Log.e(tag, mPump.toString());
     }
 }
